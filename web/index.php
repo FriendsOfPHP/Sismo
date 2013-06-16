ce9a7c9e4a87845011cd29990e02815e42bee579
Grégoire Pineau <lyrixx@lyrixx.info>
2013-06-16 04:31:00 +0200

    Check for autoload elsewhere in case Sismo is a dependency (web part)

--- web/index.php
+++ web/index.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+if (is_file(__DIR__.'/../vendor/autoload.php')) {
+    require_once __DIR__.'/../vendor/autoload.php';
+} else if (is_file(__DIR__.'/../../../autoload.php')) {
+    require_once __DIR__.'/../../../autoload.php';
+}
+
 $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../src/controllers.php';
 $app->run();
