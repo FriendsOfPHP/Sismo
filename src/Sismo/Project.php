472786a48d32fd2f1954e362bfb9de1d62732cfe
Simon Yorkston <simon.yorkston@gmail.com>
2013-08-07 07:07:00 +1000

    Final Regex Update
    System now accepts SSH URLs and Github SSH Urls

--- src/Sismo/Project.php
+++ src/Sismo/Project.php
@@ -296,13 +296,23 @@ class Project
      */
     public function setRepository($url)
     {
-        if (false !== strpos($url, '@')) {
-            list($url, $branch) = array_pop(explode('@', $url));
-            $this->branch = $branch;
-        }
+	//If we have a URL that is an SSH URL - we will split it down into its elements via preg_match
+	//If not, we'll explode it on an @ to get the branch
+	if (preg_match('#^(([a-z0-9_-]+)?@[^:]+:[a-z0-9_-]+/[a-z0-9_-]+\.git)@?([a-z0-9_-]+)?$#i',$url,$elements) === 1) {
 
-        $this->repository = $url;
+	    $this->repository = $elements[1];
+	    if (!empty($elements[3])) {
+	    	$this->branch = $elements[3];
+	    }
 
+        } else {
+	        if (false !== strpos($url, '@')) {
+        		list($url, $branch) = explode('@', $url);
+           		$this->branch = $branch;
+        	}
+
+        	$this->repository = $url;
+	}
         return $this;
     }
 
